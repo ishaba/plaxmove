98fcd2e32494b59eddde86334c24c2afa70b30dc
Ilya <ishabanov509@gmail.com>
2013-07-22 08:35:45 -0700

    Update jquery.plaxmove.js
    a bit refactoring

--- jquery.plaxmove.js
+++ jquery.plaxmove.js
@@ -4,8 +4,6 @@
 		this.defaults = {
 			ratioH: 0.2,
 			ratioV: 0.2,
-			invertH: false,
-			invertV: false,
 			reversed: false
 		}
 		
@@ -18,50 +16,16 @@
 			y0 = layer.offset().top,
 			x0 = layer.offset().left;
 			
-		if(settings.reversed) {
-
-			if(settings.invertH)
-				var eqH = function(e) {
-					return x0-(e.pageY - center.y)*settings.ratioH
-				}
-			
-			else 
-				var eqH = function(e) {
-					return x0+(e.pageY - center.y)*settings.ratioH
-				}
-			
-			if(settings.invertV)
-				var eqW = function(e) {
-					return y0-(e.pageX - center.x)*settings.ratioV
-				}
-			else
-				var eqW = function(e) {
-					return y0+(e.pageX - center.x)*settings.ratioV
-				}
-
+		var eqH = function(e) {
+			return x0+(e.pageX - center.x)*settings.ratioH
 		}
-		
-		else {
 
-			if(settings.invertH)
-				var eqH = function(e) {
-					return x0-(e.pageX - center.x)*settings.ratioH
-				}
-			
-			else 
-				var eqH = function(e) {
-					return x0+(e.pageX - center.x)*settings.ratioH
-				}
-			
-			if(settings.invertV)
-				var eqW = function(e) {
-					return y0-(e.pageY - center.y)*settings.ratioV
-				}
-			else
-				var eqW = function(e) {
-					return y0+(e.pageY - center.y)*settings.ratioV
-				}		
+		var eqW = function(e) {
+			return y0+(e.pageY - center.y)*settings.ratioV
+		}        
 
+		if(settings.reversed) {
+			var t = eqH; eqH = eqW; eqW = t;
 		}
 
 		$('html').on('mousemove', function(e) {
